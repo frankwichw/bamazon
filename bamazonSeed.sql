00219144406176994a84386ce6a1c11d9a3d2579
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2018-02-09 19:15:11 -0500

    bamazon seeds

---
+++ bamazonSeed.sql
@@ -0,0 +1,29 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Candles (20 pck)", "Home Goods", 12.99, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Mystical Wizard Blacklight Poster", "Home Goods", 15.00, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Lamb Stuffed Animal", "Toys", 8.50, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("iPod Nano (2nd Generation)", "Electronics", 33.00, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Cutlery Set (18pc)", "Home Goods", 88.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Blonde Wig", "Beauty and Health", 30.00, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Return of the King (used)", "Books", 5.99, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Goblin Figurine (10 inch)", "Toys", 15.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Standing Fan", "Electronics", 14.00, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Fur Coat", "Clothing", 350.00, 1);
