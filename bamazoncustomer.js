27b113a7428cb3aa9ef7bf817bbe8eedcb4eef9c
frankwichw <frankwichw@gmail.com>
2018-02-14 15:57:45 -0500

    Customer View

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,57 +1,119 @@
+// requiring node packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var userInput = process.argv[2];
-var userInputTwo = process.argv[3];
+// initializing variable so they are available globally
+var productID;
+var productUnitCount;
+var warehouseCount;
 
-// connection variable with details of connection
+// connection variable with port/username/password/etc.
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "root",
   database: "bamazon"
 });
 
-// function to connect 
+// this function connects user upon the file being loaded in node
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
 });
 
+// starting function, called at the bottom of the file
 var startup = function(){
+  // query that selects all (denoted by *) rows from the products table
 	connection.query("SELECT * FROM products", 
 		function(err, res) {
 		if (err) throw err;
-		// Log all results of the SELECT statement
+		// for loop that loops through results and console logs out products and information about them
 		for (var i = 0; i < 10; i++){
 			console.log("\nItem ID: " + res[i].item_id + " | Product: " + res[i].product_name + " | Price: $" + res[i].price);
     }
+    // console log to give an extra space beneath the products without having to add that space to each looped console log
     console.log("\n");
-    // connection.end();
+    // calling next function
     buyOptions();
   });
   
 };
 
+// function to house inquirer prompts
 var buyOptions = function(){
   inquirer.prompt(
   	{
   	type: "input",
   	name: "ID",
-  	message: "What is the item ID of the item you would like to buy? (Type just the ID and hit enter)"
+  	message: "What is the item ID of the item you would like to buy?"
   	}).then(function(answers) {
-  		if (parseInt(answers.ID) < 11){
-        console.log(answers.ID);
+      // if/else statement checking that the entered number is valid as an item_id
+  		if (parseInt(answers.ID) < 11 && parseInt(answers.ID) >= 0){
+        // setting global variable to property of answers object
+        productID = answers.ID;
+
+        inquirer.prompt(
+          {
+          type: "input",
+          name: "units",
+          message: "How many units of this item would you like to buy?"
+          }).then(function(answers) {
+            // setting global variable and calling next function on success
+            productUnitCount = answers.units;
+            fulfillOrder();
+        });
       } else {
+        // otherwise console logging an error message and ending the connection
         console.log("Please enter a valid ID");
+        connection.end();
       };
   });
-  connection.end();
 };
+
+// function to validate that there are enough units to buy
+var fulfillOrder = function(){
+  // console logs current order information for user
+  console.log("\nYour current order:\nID: " + productID + "\nNumber of units: " + productUnitCount + "\n");
+  // query that selects the stock_quantity column from item_id equal to the ID the customer previously entered
+  connection.query("SELECT stock_quantity FROM products WHERE ?", 
+    {
+      item_id: productID
+    },
+		function(err, res) {
+      // setting global variable with result object
+      warehouseCount = res[0].stock_quantity;
+
+      // if there are more than, or the same number of units, call next function, otherwise console log error
+      if (warehouseCount >= productUnitCount){
+        updateUnits();
+      } else {
+        console.log("\nInsufficient Quantity!\n");
+      }
+  });
+}
+
+// function to update the units according to how many were bought
+var updateUnits = function(){
+  // setting variable to the new number of units
+  var newQuantity = warehouseCount - productUnitCount;
+  // updating stock quantity 
+  connection.query("UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: newQuantity
+      },
+      {
+        item_id: productID
+      }
+    ],
+    function(err, res) {
+      console.log("\nYour order has been processed! Allow 5-24 days for shipping.\n");
+    }
+  );
+  connection.end();
+}
 
 startup();
